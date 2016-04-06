a8148da211ca635fe7d6057e1e1f9eda1c70ae6a
Jason Lam <jasonlam@melco-crown.com>
2016-04-06 16:40:50 +0800

    testing merge

---
+++ poc/src/central/public/js/conversion.js
@@ -0,0 +1,68 @@
+var courtesyName = {
+	dnd: "Do Not Disturb",
+	makeuproom: "Makeup Room",
+	laundry: "Laundry",
+	bell: "Bell"
+}
+
+var lightingName = {
+	foyerdownlight: "Foyer Downlight",
+	bedroomdownlight: "Bedroom Downlight",
+	bathroommirrorlight: "Bathroom Mirror Light",
+	bathroomdownlight: "Bathroom Downlight",
+	closetdownlight: "Closet Downlight",
+	rightpendentlamp: "Right Pendent Lamp",
+	leftpendentlamp: "Left Pendent Lamp",
+	nightlight: "Night Light",
+	doghouselight: "Dog House Light",
+	dimmerlight: "Dimmer Light"
+}
+
+var selfserviceName = {
+	checkout: "Checkout",
+	roomservice: "Room Service",
+	teatime: "Tea Time",
+	wakeupcall: "Wakeup Call"
+}
+
+var stateName = {
+	READY: 'Ready',
+	INACTIVE: 'Inactive',
+	DISABLED: 'Disabled',
+	ACTIVE: 'Active',
+	CHECKIN: 'Check-in',
+	CHECKOUT: 'Check-out',
+	HOUSEKEEP: 'Housekeep'
+}
+
+var lightingDimmableName = {
+	true: "Yes",
+	false: "No"
+}
+
+var hvacModeName = {
+	HEATER: 'Heater',
+	AIRCON: 'Air Conditioner',
+	OFF: 'Off'
+}
+
+var hvacFanspeedName = {
+	LOW: 'Low',
+	MID: 'Mid',
+	HIGH: 'High'
+}
+
+var curtainModeName = {
+	OPEN: 'Open',
+	CLOSE: 'Close'
+}
+
+var wakeupCallPowerName = {
+	ON: 'Yes',
+	OFF: 'No'
+}
+
+var selfCheckoutName = {
+	YES: 'Yes',
+	NO: 'No'
+}
---
+++ poc/src/central/public/js/myjs.js
@@ -0,0 +1,3 @@
+function alertbox(){
+	alert('hihi');
+}
